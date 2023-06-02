2091296d857cef11a2872b0b187f00b99f8c1735
Ilyas Rufai <rufaiilyas@gmail.com>
2023-06-02 09:21:08 +0100

    feat: config db

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',  # This is the name of the PostgreSQL service in my Docker Compose file
+        'PORT': '5234',  # This will be the port mapped to the PostgreSQL service in the Docker Compose file
     }
 }
 
