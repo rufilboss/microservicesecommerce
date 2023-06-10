b1bc7b512d27ea7065044df09b37e00feb81f082
Ilyas Rufai <rufaiilyas@gmail.com>
2023-06-10 21:15:21 +0100

    feat: config db

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -83,34 +83,34 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'test',
+#         'NAME': 'db',
 #         'USER': 'postgres',
-#         'PASSWORD': 'test123',
+#         'PASSWORD': 'postgres',
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',  # This is the name of the PostgreSQL service in my Docker Compose file
-        'PORT': '5432',  # This will be the port mapped to the PostgreSQL service in the Docker Compose file
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',  # This is the name of the PostgreSQL service in my Docker Compose file
+#         'PORT': '5432',  # This will be the port mapped to the PostgreSQL service in the Docker Compose file
+#     }
+# }
 
 
 # Password validation
